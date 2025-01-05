100be956139a25a19dc7d976066f7dce18deb590
Mariraj <maari000@hotmail.com>
2025-01-05 22:35:11 +0530

    temp

--- util/database.js
+++ util/database.js
@@ -4,16 +4,6 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.NODE_ENV === "production") {
-    sequelize = new Sequelize(process.env.DATABASE_URL, {
-        dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-        },
-    });
-} else {
     sequelize = new Sequelize(
         process.env.DB_SCHEMA,
         process.env.DB_USERNAME,
@@ -21,6 +11,17 @@ if (process.env.NODE_ENV === "production") {
         {
             host: process.env.DB_HOST,
             dialect: 'mysql',
+            port: 3306
+        }
+    );
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_SCHEMA,
+        process.env.DB_USERNAME,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT,
         }
     );
 }

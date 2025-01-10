a700c32c9da40e05794662e0d450e8f89df5fbac
Mariraj <maari000@hotmail.com>
2025-01-10 18:42:02 +0530

    temp

--- util/database.js
+++ util/database.js
@@ -5,13 +5,14 @@ let sequelize;
 
 if (process.env.NODE_ENV === "production") {
     sequelize = new Sequelize(
-        process.env.DB_SCHEMA,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,
+        process.env.INF_DB_SCHEMA,
+        process.env.INF_DB_USERNAME,
+        process.env.INF_DB_PASSWORD,
         {
-            host: process.env.DB_HOST,
-            dialect: 'mysql',
-            port: 3306
+            host: process.env.INF_DB_HOST,
+            dialect: "mysql",
+            port: 3306,
+            logging: false,
         }
     );
 } else {
@@ -21,7 +22,8 @@ if (process.env.NODE_ENV === "production") {
         process.env.DB_PASSWORD,
         {
             host: process.env.DB_HOST,
-            dialect: process.env.DB_DIALECT,
+            dialect: process.env.DB_DIALECT || "mysql",
+            logging: true,
         }
     );
 }

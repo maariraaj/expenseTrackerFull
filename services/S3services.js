1311485cf86bb46388903b8c1069047feab6ebd3
Mariraj <maari000@hotmail.com>
2025-01-15 21:07:32 +0530

    sdk v3 upgrade

--- services/S3services.js
+++ services/S3services.js
@@ -1,29 +1,63 @@
-const AWS = require('aws-sdk');
+// const AWS = require('aws-sdk');
+// require('dotenv').config();
+
+// exports.uploadToS3 = (data, fileName) => {
+//     const BUCKET_NAME = process.env.BUCKET_NAME;
+//     const IAM_USER_KEY = process.env.IAM_USER_KEY;
+//     const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
+//     let s3bucket = new AWS.S3({
+//         accessKeyId: IAM_USER_KEY,
+//         secretAccessKey: IAM_USER_SECRET
+//     });
+//     let params = {
+//         Bucket: BUCKET_NAME,
+//         Key: fileName,
+//         Body: data,
+//         ACL: 'public-read'
+//     }
+//     return new Promise((resolve, reject) => {
+//         s3bucket.upload(params, (err, s3response) => {
+//             if (err) {
+//                 console.log('Something went wrong', err);
+//                 reject(err);
+//             } else {
+//                 console.log('success', s3response);
+//                 resolve(s3response.Location);
+//             }
+//         });
+//     });
+// };
+
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 require('dotenv').config();
 
-exports.uploadToS3 = (data, fileName) => {
+exports.uploadToS3 = async (data, fileName) => {
     const BUCKET_NAME = process.env.BUCKET_NAME;
     const IAM_USER_KEY = process.env.IAM_USER_KEY;
     const IAM_USER_SECRET = process.env.IAM_USER_SECRET;
-    let s3bucket = new AWS.S3({
-        accessKeyId: IAM_USER_KEY,
-        secretAccessKey: IAM_USER_SECRET
+
+    const s3Client = new S3Client({
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: IAM_USER_KEY,
+            secretAccessKey: IAM_USER_SECRET,
+        },
     });
-    let params = {
+
+    const params = {
         Bucket: BUCKET_NAME,
         Key: fileName,
         Body: data,
-        ACL: 'public-read'
+        ACL: 'public-read',
+    };
+
+    try {
+        const command = new PutObjectCommand(params);
+        const response = await s3Client.send(command);
+        console.log('Success:', response);
+        return `https://${BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+    } catch (err) {
+        console.error('Something went wrong:', err);
+        throw err;
     }
-    return new Promise((resolve, reject) => {
-        s3bucket.upload(params, (err, s3response) => {
-            if (err) {
-                console.log('Something went wrong', err);
-                reject(err);
-            } else {
-                console.log('success', s3response);
-                resolve(s3response.Location);
-            }
-        });
-    });
 };
